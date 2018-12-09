16ce70bcb0dae0376c5d268359b077854056e23f
Taylor Perkins <taylorperkins@Taylors-Juicy-Macbook-Pro.local>
2018-12-09 15:00:31 -0600

    Comments for day 6 and 7

--- 7/solution_pt1.py
+++ 7/solution_pt1.py
@@ -9,6 +9,7 @@ class Step(object):
         self.finished = True
 
     def __repr__(self):
+        """Helps me debug a bit"""
         return self.name
 
 
@@ -19,6 +20,7 @@ class OrderOfOps(object):
         self.finished_steps = list()
 
     def add_step(self, step, relies_on=''):
+        """Just build the steps and specify if they rely on another step before being started"""
         if step not in self.steps:
             self.steps[step] = Step(step)
 
@@ -27,15 +29,20 @@ class OrderOfOps(object):
             self.steps[step].finished = False
 
     def determine_order(self):
+        # so you can track when youre done
         original_step_len = len(self.steps)
 
         while len(self.finished_steps) != original_step_len:
+            # Alphabetically sort (important)
             for key in sorted(self.steps):
                 if self.steps[key].finished:
+                    # marks everything appropriately so the next iteration picks up on the changes
                     self.finished_steps.append(key)
 
                     del self.steps[key]
 
+                    # Since we just finished a step, it's important to go through the other steps specify that
+                    # they no longer rely on the finished step
                     for step in self.steps.values():
                         if key in step.relies_on:
                             step.relies_on.remove(key)
@@ -44,19 +51,26 @@ class OrderOfOps(object):
 
                     break
 
+        # all there is
         return ''.join(self.finished_steps)
 
 
 if __name__ == '__main__':
+    """It was a bit tricky to figure this one out. It took a bit of brainpower to map out the "business rules" for 
+    this problem.
+    """
     with open('./input.txt') as f:
         data = f.read().splitlines()
 
     order_of_ops = OrderOfOps()
 
+    # build out all of the steps and which ones require what. Then you can determine what order they need to
+    # happen to be finished.
     pattern = re.compile(r'Step (?P<relies_on>[A-Z]) must be finished before step (?P<step>[A-Z])')
     for line in data:
         m = re.match(pattern, line)
 
+        # two steps are either created or referenced in every line of the input.. so you have to create or update both
         step = m.group('step')
         relies_on = m.group('relies_on')
 
--- 7/solution_pt2.py
+++ 7/solution_pt2.py
@@ -24,27 +24,30 @@ class Step(object):
 class Workers(object):
     def __init__(self, num_workers):
         self.available_workers = num_workers
-        self.status = 'available'
 
         self.work_load = list()
 
     def pick_up_task(self, step):
+        # pass a step to a worker, given that there is one just sitting around
         if self.available_workers:
             self.work_load.append(step)
             self.available_workers -= 1
-            self.status = 'busy'
 
     def work_on_tasks(self):
+        # whip it. Create a new list now that a second has passed based on step status updates
         updated_workload = list()
         finished_steps = list()
 
         for step in self.work_load:
+            # update the steps second requirement
             step.seconds_to_complete -= 1
 
             # it's finished
             if not step.seconds_to_complete:
                 step.finished = True
                 finished_steps.append(step.name)
+
+                # unassign the step from the worker
                 self.available_workers += 1
 
             else:
@@ -66,6 +69,7 @@ class OrderOfOps(object):
         self.steps = dict()
         self.finished_steps = list()
 
+        # Outsource ittttt
         self.workers = Workers(num_workers=num_workers)
 
     def add_step(self, step, relies_on=''):
@@ -77,35 +81,49 @@ class OrderOfOps(object):
             self.steps[step].available = False
 
     def determine_order(self):
+        """This is pretty similar to pt 1, but now it passes off a lot of status handling to a worker to deal with.
+        It keeps going until it has gone through all of the steps."""
         original_step_len = len(self.steps)
 
+        # initial second count
         seconds = 0
         while len(self.finished_steps) != original_step_len:
+            # need to check if I even have workers I can use
             if self.workers.workers_available():
+                # see if the workers have finished any steps
                 available_steps = self.get_available_steps()
 
                 for step in available_steps:
                     if step.name not in self.workers.get_work_queue() and self.workers.workers_available():
+                        # if there is a step that can be worked on, and workers are available, pass off the step
                         self.workers.pick_up_task(step)
 
+            # always make sure the worker does what hes supposed to
             self.workers.work_on_tasks()
+
+            # make sure that for any step that just got finished, the other steps no longer rely on it.
+            # Also removes the finished step from the queue
             self.finish_steps()
             seconds += 1
 
         return ''.join(self.finished_steps), seconds
 
     def finish_steps(self):
+        # marks everything appropriately so the next iteration picks up on the changes
         for key, step in self.steps.items():
             if step.finished:
                 self.finished_steps.append(key)
 
+                # take it out of the queue
                 del self.steps[key]
 
-                # remove all instances of steps that rely on the finished key
+                # Since we just finished a step, it's important to go through the other steps specify that
+                # they no longer rely on the finished step
                 for step in self.steps.values():
                     if key in step.relies_on:
                         step.relies_on.remove(key)
 
+                        # mark it as available for a worker to pick up
                         if not step.relies_on:
                             step.available = True
 
@@ -116,6 +134,13 @@ class OrderOfOps(object):
 
 
 if __name__ == '__main__':
+    """This one was definitely more complicated than the first one. There are a lott of moving parts.. So 
+    I decided to really separate it into each individual piece: Step, Workers, and OrderOfOps
+    
+    Each step really only handles maintaining different statuses.
+    Workers are responsible for updating the steps statuses as seconds go by.
+    OrderOfOps is responsible for supplying the workers with available steps to complete and moving the steps along.
+    """
     with open('./input.txt') as f:
         data = f.read().splitlines()
 
