abd1c7d52fa82f3b48feaf6dcf15c01afb4f9ab8
Taylor Perkins <taylorperkins@Taylors-Juicy-Macbook-Pro.local>
2018-12-09 14:00:22 -0600

    comments for the second day

--- 2/solution_pt1.py
+++ 2/solution_pt1.py
@@ -2,6 +2,12 @@ from collections import Counter
 
 
 if __name__ == '__main__':
+    """Here, we are basically categorizing lines in a file: if they contain a letter two or three times.
+    
+    The final result is the number of lines that contain 2 of the same letters multiplied by the 
+    umber of lines that contain 3 of the same letter.
+    """
+
     twos = 0
     threes = 0
 
@@ -9,17 +15,15 @@ if __name__ == '__main__':
         for line in f.readlines():
             line_counter = Counter()
 
+            has_two = False
+            has_three = False
             for letter in line:
                 line_counter[letter] += 1
 
-            has_two = False
-            has_three = False
-
-            for value in line_counter.values():
-                if value == 2:
+                if line_counter[letter] == 2:
                     has_two = True
 
-                elif value == 3:
+                elif line_counter[letter] == 3:
                     has_three = True
 
             if has_two:
--- 2/solution_pt2.py
+++ 2/solution_pt2.py
@@ -1,17 +1,26 @@
 if __name__ == '__main__':
+    """Had to take a step back for this one! 
+    
+    We are comparing each line to one another and keeping track of the same values at each index.
+    For the lines that are most similar, what are the values between the two lines that are the same?
+    """
+
     with open('./input.txt', 'r') as f:
         data = f.read().splitlines()
 
     final_diff_indices, final_minus_diff = list(), ''
 
     data_len = len(data)
+
+    # I dont want to do multiple checks, so there is this nestted loop
     for i, v1 in enumerate(data):
-        if i + 1 != data_len:
+        if i + 1 != data_len:  # next value exists..
             for v2 in data[i + 1:]:
 
                 minus_diff = ''
                 diff_indices = list()
 
+                # check values at each corresponding index, and record the findings
                 for ind, v1_letter in enumerate(v1):
                     v2_letter = v2[ind]
 
@@ -20,6 +29,7 @@ if __name__ == '__main__':
                     else:
                         diff_indices.append(ind)
 
+                # keep track of the smallest comparison
                 if not final_diff_indices or len(diff_indices) < len(final_diff_indices):
                     final_diff_indices = diff_indices
                     final_minus_diff = minus_diff
